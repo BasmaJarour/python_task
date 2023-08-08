ad9e8393a122705cbf359bf1fbba066a4fbc40d0
BasmaJarour <57730714+BasmaJarour@users.noreply.github.com>
2023-08-08 19:29:06 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,9 @@
+
+
+inpu = input("What is your current age? ")
+age=int(inpu)
+yearRemain=90-age
+days=yearRemain*365
+week=yearRemain*52
+month=yearRemain*12
+print(f"you have {days} days, {week} weeks, and {month} months ,{yearRemain} years left.")
